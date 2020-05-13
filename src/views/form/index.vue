d8e38dec142b46411412fa0356f589186e210324
BJ0815 <jtt11220@gmail.com>
2020-05-13 14:57:24 +0800

    ✨ Add Form with tinymce and upload image

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="app-container">
-    <el-form ref="form" :model="form" label-width="120px">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name" />
+    <el-form ref="form" :model="form" :rules="rules" label-width="120px">
+      <el-form-item label="Title" prop="title">
+        <el-input v-model="form.title" required />
       </el-form-item>
-      <el-form-item label="Activity zone">
-        <el-select v-model="form.region" placeholder="please select your zone">
-          <el-option label="Zone one" value="shanghai" />
-          <el-option label="Zone two" value="beijing" />
+      <el-form-item label="Category">
+        <el-select v-model="form.category" placeholder="please select your zone">
+          <el-option label="News" value="news" />
+          <el-option label="Products" value="products" />
         </el-select>
       </el-form-item>
       <el-form-item label="Activity time">
@@ -19,59 +19,80 @@
           <el-time-picker v-model="form.date2" type="fixed-time" placeholder="Pick a time" style="width: 100%;" />
         </el-col>
       </el-form-item>
-      <el-form-item label="Instant delivery">
-        <el-switch v-model="form.delivery" />
+      <el-form-item prop="content" label="Content" style="margin-bottom: 30px;">
+        <Tinymce ref="editor" v-model="form.content" :height="400" />
       </el-form-item>
-      <el-form-item label="Activity type">
-        <el-checkbox-group v-model="form.type">
-          <el-checkbox label="Online activities" name="type" />
-          <el-checkbox label="Promotion activities" name="type" />
-          <el-checkbox label="Offline activities" name="type" />
-          <el-checkbox label="Simple brand exposure" name="type" />
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources">
-        <el-radio-group v-model="form.resource">
-          <el-radio label="Sponsor" />
-          <el-radio label="Venue" />
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form">
-        <el-input v-model="form.desc" type="textarea" />
+      <el-form-item prop="image_uri" label="Photos" style="margin-bottom: 30px;">
+        <Upload v-model="form.image_uri" />
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">Create</el-button>
-        <el-button @click="onCancel">Cancel</el-button>
+        <el-button v-loading="loading" type="primary" @click="onSubmit">Create</el-button>
+        <el-button v-loading="loading" @click="onCancel">Cancel</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+import Tinymce from '@/components/Tinymce'
+import Upload from '@/components/Upload/SingleImage3'
+
 export default {
+  components: {
+    Tinymce,
+    Upload
+  },
   data() {
+    const validateRequire = (rule, value, callback) => {
+      if (value === '') {
+        this.$message({
+          message: rule.field + '為必填',
+          type: 'error'
+        })
+        callback(new Error(rule.field + '為必填'))
+      } else {
+        callback()
+      }
+    }
+
     return {
       form: {
-        name: '',
-        region: '',
+        title: '',
+        category: 'news',
         date1: '',
         date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+        image_uri: '',
+        content: ''
+      },
+      loading: false,
+      rules: {
+        image_uri: [{ validator: validateRequire }],
+        title: [{ validator: validateRequire }],
+        content: [{ validator: validateRequire }]
       }
     }
   },
   methods: {
     onSubmit() {
-      this.$message('submit!')
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.loading = true
+
+          this.$notify({
+            title: '成功',
+            message: '建立文章完成',
+            type: 'success',
+            duration: 2000
+          })
+          this.loading = false
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
     },
     onCancel() {
-      this.$message({
-        message: 'cancel!',
-        type: 'warning'
-      })
+      this.$router.replace('/dashboard')
     }
   }
 }
